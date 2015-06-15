d59dbb5fb163e1d925159d534469e91dfcb353d1
Alex Schastny <to_alex@tut.by>
2015-06-15 17:19:17 +0300

    added reverse on second match

--- web/assets/matchForm.js
+++ web/assets/matchForm.js
@@ -3,6 +3,7 @@ var MatchForm = function (target, isTournament) {
     this.target = target;
     this.template = _.template($('#match-form-template').html());
     this.bind();
+    this.round = 0;
 };
 
 MatchForm.prototype = {
@@ -13,15 +14,21 @@ MatchForm.prototype = {
 
     bind: function() {
         var self = this;
-        $('#page').on('submit', 'form', function(e){
+        this.target.on('submit', 'form', function(e){
             var form = $(e.currentTarget);
+
+            self.round++;
 
             $(this).ajaxSubmit({
                 dataType: 'json',
                 success: function(response) {
                     if (response && response.success) {
                         if (!self.isTournament) {
-                            self.clearForm(form);
+                            if (self.round == 1) {
+                                self.reverseForm(form);
+                            } else {
+                                self.clearForm(form);
+                            }
                         } else {
                             self.blockForm(form);
                         }
@@ -29,6 +36,10 @@ MatchForm.prototype = {
                 }
             });
             return false;
+        });
+
+        this.target.on('change', 'select', function() {
+
         });
     },
 
@@ -39,6 +50,24 @@ MatchForm.prototype = {
 
     clearForm: function(form) {
         form.find('select [value=0]').prop('selected', true);
+        form.find('input[type="text"]').val('');
+        form.css('background-color', '#5cb85c');
+
+        setInterval(function() {
+            form.css('background-color', 'white');
+        }, 1500)
+    },
+
+    reverseForm: function(form) {
+        var tempGoalkeeper = form.find('select[name="match[red_goalkeeper]"]').val();
+
+        form.find('select[name="match[red_goalkeeper]"]').val(form.find('select[name="match[red_forward]"]').val());
+        form.find('select[name="match[red_forward]"]').val(tempGoalkeeper);
+
+        tempGoalkeeper = form.find('select[name="match[blue_goalkeeper]"]').val();
+        form.find('select[name="match[blue_goalkeeper]"]').val(form.find('select[name="match[blue_forward]"]').val());
+        form.find('select[name="match[blue_forward]"]').val(tempGoalkeeper);
+
         form.find('input[type="text"]').val('');
         form.css('background-color', '#5cb85c');
 
