04c881f42dccc2ab9bbcc664a9b6e047807d466a
Alex Schastny <to_alex@tut.by>
2015-08-14 13:38:28 +0300

    #20 on user select on history, show daily change rating

--- web/assets/historyController.js
+++ web/assets/historyController.js
@@ -1,17 +1,25 @@
 var HistoryController = function () {
     this.bind();
-    console.log(2);
 };
 
 HistoryController.prototype = {
     bind: function () {
         $('.highlight-player').on('change', 'select', function(e){
             var player = $(e.currentTarget).val();
+            $('h4 .day-change').removeClass('rating-positive').removeClass('rating-negative').html('');
             $('table tr').removeClass('success').removeClass('danger');
             var tds = $('table').find('td[player="' + player + '"]');
+            var totalDayChange = {};
 
             _.each(tds, function (td) {
                 td = $(td);
+
+                if (_.isUndefined(totalDayChange[td.closest('table').data('day')])) {
+                    totalDayChange[td.closest('table').data('day')] = 0;
+                }
+
+                totalDayChange[td.closest('table').data('day')] += parseFloat(td.data('rating-change'));
+
                 var tr = td.closest('tr');
 
                 if (tr.find('.' + td.data('command')).data('score') == 5) {
@@ -19,6 +27,13 @@ HistoryController.prototype = {
                 } else {
                     tr.addClass('danger');
                 }
+            });
+
+            var dayChange;
+            _.each(totalDayChange, function(value, index){
+                dayChange = $('h4[title-day="' + index + '"] .day-change');
+                dayChange.html('(' + Math.round(value * 100) / 100 + ')');
+                dayChange.addClass(value > 0 ? 'rating-positive' : 'rating-negative');
             });
         });
     }
